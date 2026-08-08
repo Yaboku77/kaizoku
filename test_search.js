aa52bb8decdfeb7491d4b990bfebed6b21b8a1da
Yaboku77 <90348922+Yaboku77@users.noreply.github.com>
2026-08-08 11:38:49 +0530

    Delete test_search.js

--- test_search.js
+++
@@ -1,8 +0,0 @@
-const { scrapeSearch } = require('./src/api/scrapers/search.scraper');
-
-async function test() {
-  const data = await scrapeSearch("Attack on Titan");
-  console.log(JSON.stringify(data, null, 2));
-}
-
-test();
