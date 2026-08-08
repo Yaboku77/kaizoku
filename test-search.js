0ef3168e86e8e86f3947f326623f7cde8bce80dd
Yaboku77 <90348922+Yaboku77@users.noreply.github.com>
2026-08-08 11:37:05 +0530

    Delete test-search.js

--- test-search.js
+++
@@ -1,8 +0,0 @@
-const { scrapeSearch } = require('./src/api/scrapers/search.scraper');
-
-async function test() {
-  const data = await scrapeSearch('Jujutsu Kaisen');
-  console.log(JSON.stringify(data.results.map(r => ({ title: r.title, titleJp: r.titleJp, slug: r.slug })), null, 2));
-}
-
-test().catch(console.error);
